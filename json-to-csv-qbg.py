1b14c849b6c9bf61429bf959b4eb5d341b2a69a2
sjpark-mcci <sjpark@sjpark-note2.localdomain>
2019-11-04 00:56:41 -0500

    update header comment

--- json-to-csv-qbg.py
+++ json-to-csv-qbg.py
@@ -2,7 +2,7 @@
 # Module: json-to-csv-qbg.py
 #
 # Function:
-#	Extract sensor data from a JSON file as CSV for QBG
+#	Convert sensor data from a JSON file to CSV for QBG
 #
 # Copyright:
 #	See LICENSE file.
@@ -11,8 +11,8 @@
 #	Sungjoon Park, MCCI Corporation
 #
 # Description:
-#	This function extracts data from an influxdb JSON file generated
-#	from the WeRadiate data set for a given sensor.
+#	This function convert data from an influxdb JSON file generated
+#	from the WeRadiate data set for a given sensor to a CSV file.
 
 import json
 import csv
